bfcb17bde249c03a9cc6ec6fe83f7d6b0a68f04b
DonGitSchool <92173334+DonGitSchool@users.noreply.github.com>
2022-02-16 17:22:28 -0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,35 +1,65 @@
-let wifistate = false
-let farenheit = 0
-input.onButtonPressed(Button.A, function () {
-    wifistate = true
-    WiFiBit.connectToWiFiBit()
-})
-function convert_to_f (temp: string) {
-    farenheit = parseInt(temp) - 273.15 * 9 / 5 + 32
+def on_pin_pressed_p0():
+    basic.show_icon(IconNames.HEART)
+    basic.show_string("Temp is: ", temp2)
+    basic.show_string("Hello!")
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+
+def on_button_pressed_a():
+    global wifistate
+    wifistate = True
+    WiFiBit.connect_to_wi_fi_bit()
+    strip.show()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def convert_to_f(temp: str):
+    global farenheit
+    farenheit = int(temp) - 273.15 * 9 / 5 + 32
     return farenheit
-}
-input.onButtonPressed(Button.B, function () {
-    wifistate = false
-})
-basic.forever(function () {
-    if (wifistate == true) {
-        basic.showLeds(`
+
+def on_button_pressed_b():
+    global wifistate
+    wifistate = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+farenheit = 0
+wifistate = False
+strip: neopixel.Strip = None
+temp2 = 0
+strip = neopixel.create(DigitalPin.P2, 8, NeoPixelMode.RGB)
+temp2 = int(pins.analog_read_pin(AnalogPin.P0))
+
+def on_forever():
+    if wifistate == True:
+        basic.show_leds("""
             . . . . .
-            # . . . #
-            # . # . #
-            # . # . #
-            . # . # .
-            `)
-        WiFiBit.connectToWiFiNetwork("SSID", "key")
-    }
-    if (wifistate == false) {
-        basic.showLeds(`
+                        # . . . #
+                        # . # . #
+                        # . # . #
+                        . # . # .
+        """)
+    if wifistate == False:
+        basic.show_leds("""
             . . . . .
-            . # . # .
-            # . # . #
-            # . # . #
-            # . . . #
-            `)
-        WiFiBit.disconnectFromWiFiNetwork()
-    }
-})
+                        . # . # .
+                        # . # . #
+                        # . # . #
+                        # . . . #
+        """)
+    if farenheit > 70:
+        wifistate2 = False
+        WiFiBit.disconnect_from_wi_fi_network()
+        music.start_melody(music.built_in_melody(Melodies.RINGTONE), MelodyOptions.ONCE)
+    elif farenheit > 90:
+        wifistate2 = False
+        WiFiBit.disconnect_from_wi_fi_network()
+        music.start_melody(music.built_in_melody(Melodies.ENTERTAINER),
+            MelodyOptions.ONCE)
+    for index in range(25):
+            strip.set_brightness(pins.map(pins.analog_read_pin(AnalogPin.P1), 300, 500, 1, 100))
+            strip.set_pixel_color(index,
+                neopixel.rgb(Math.random_range(0, 254),
+                    Math.random_range(0, 254),
+                    Math.random_range(0, 254)))
+            strip.show()
+            basic.pause(100)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showString("Temp is: ", temp2)
+basic.showString("Hello!")
+})
 input.onButtonPressed(Button.A, function () {
     wifistate = true
     WiFiBit.connectToWiFiBit()
+    strip.show()
 })
 function convert_to_f (temp: string) {
     farenheit = parseInt(temp) - 273.15 * 9 / 5 + 32
@@ -11,9 +17,13 @@ input.onButtonPressed(Button.B, function () {
 })
 let farenheit = 0
 let wifistate = false
-let temp = pins.analogReadPin(AnalogPin.P0)
+let strip: neopixel.Strip = null
+let temp2 = 0
+strip = neopixel.create(DigitalPin.P2, 8, NeoPixelMode.RGB)
+temp2 = Math.trunc(pins.analogReadPin(AnalogPin.P0))
 basic.forever(function () {
-    if (wifistate == true) {
+    let wifistate2: boolean;
+if (wifistate == true) {
         basic.showLeds(`
             . . . . .
             # . . . #
@@ -21,7 +31,6 @@ basic.forever(function () {
             # . # . #
             . # . # .
             `)
-        WiFiBit.connectToWiFiNetwork("SSID", "key")
     }
     if (wifistate == false) {
         basic.showLeds(`
@@ -31,9 +40,26 @@ basic.forever(function () {
             # . # . #
             # . . . #
             `)
-        WiFiBit.disconnectFromWiFiNetwork()
     }
-    if (farenheit > 90) {
-    	
+    if (farenheit > 70) {
+        wifistate2 = false
+        WiFiBit.disconnectFromWiFiNetwork()
+        music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+    } else if (farenheit > 90) {
+        wifistate2 = false
+        WiFiBit.disconnectFromWiFiNetwork()
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    }
+    for (let index = 0; index <= 24; index++) {
+        strip.setBrightness(pins.map(
+        pins.analogReadPin(AnalogPin.P1),
+        300,
+        500,
+        1,
+        100
+        ))
+        strip.setPixelColor(index, neopixel.rgb(Math.randomRange(0, 254), Math.randomRange(0, 254), Math.randomRange(0, 254)))
+        strip.show()
+        basic.pause(100)
     }
 })
